3ef8bc84643ceb9dc5c2f589732c3ddb968eadb0
Dmitry Ryumkin <riumkin@gmail.com>
2019-02-13 20:17:45 +0800

    add test script

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,4 @@
 module.exports = {
-  verbose: true,
-  testURL: "http://localhost/",
-  roots: ["src"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  transformIgnorePatterns: [
-    "/node_modules/",
-    "/utilities/",
-    "/environments/"
-  ],
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-  testEnvironment: "node"
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
